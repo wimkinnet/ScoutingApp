3b9e31150cdc847a76d89ca9f349291b38172f1c
Wim Kinnet <kinnet.ceulemans@gmail.com>
2026-06-10 18:03:22 +0200

    Added device check for nav bar

--- src/Routes/AppLayout.tsx
+++ src/Routes/AppLayout.tsx
@@ -1,9 +1,11 @@
 import { Link, Outlet , useLocation} from 'react-router-dom';
 import '../styles/index.css'
 import '../styles/_tokens.css'
+import { useMediaQuery } from 'react-responsive';
 
 export default function AppLayout() {
 	const location = useLocation();
+	const isPhone = useMediaQuery({ maxWidth: 600 });
 
 	return (
 		<div className="app">
@@ -14,7 +16,7 @@ export default function AppLayout() {
 				</div>
 			</header>
 			
-			<main className="app-main">
+			{!isPhone && (<main className="app-main">
 				<aside className="app-sidebar">
 					<Link className={location.pathname.startsWith("/players") ? "sidebar-link active" : "sidebar-link"} to="/players">PLAYERS</Link>
 					<Link className={location.pathname.startsWith("/clubs") ? "sidebar-link active" : "sidebar-link"} to="/clubs">CLUBS</Link>
@@ -25,6 +27,19 @@ export default function AppLayout() {
 				</aside>
 				<Outlet />
 			</main>
+			)}
+				{isPhone && (<main className="app-phone-main">
+				<nav className="app-nav">
+					<Link className={location.pathname.startsWith("/players") ? "nav-link active" : "nav-link"} to="/players">PLAYERS</Link>
+					<Link className={location.pathname.startsWith("/clubs") ? "nav-link active" : "nav-link"} to="/clubs">CLUBS</Link>
+					<Link className={location.pathname.startsWith("/seasons") ? "nav-link active" : "nav-link"} to="/seasons">SEASONS</Link>
+					<Link className={location.pathname.startsWith("/teams") ? "nav-link active" : "nav-link"} to="/teams">TEAMS</Link>
+					<Link className={location.pathname.startsWith("/games") ? "nav-link active" : "nav-link"} to="/games">GAMES</Link>
+					<Link className={location.pathname.startsWith("/logs") ? "nav-link active" : "nav-link"} to="/logs">LOGS</Link>
+				</nav>
+				<Outlet />
+			</main>
+			)}
 		</div>
 	)
 }
